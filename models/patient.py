4a9e4a2ec854cb149131ebd1533ff357ea49925b
abdelmalektalaiche <ka_talaiche@esi.dz>
2022-07-25 18:59:26 +0100

    statue bar and tracking and many2one field

--- models/patient.py
+++ models/patient.py
@@ -6,7 +6,28 @@ class HospitalPatient(models.Model):
      _inherit = ["mail.thread","mail.activity.mixin"]
      _description = 'Hospital Patient'
 
-     name = fields.Char(string='Name', required=True)
-     age = fields.Integer(string='Age')
-     gender = fields.Selection([('male','Male'),('female','Female'),('other','Other')],required=True, default='male')
-     note = fields.Text(string='Description')
+     name = fields.Char(string='Name', tracking=True, required=True)
+     age = fields.Integer(string='Age', tracking=True)
+     gender = fields.Selection([('male','Male'),('female','Female'),('other','Other')], required=True, tracking=True, default='male')
+     note = fields.Text(string='Description', tracking=True)
+     state = fields.Selection([('draft','Draft'), ('confirm','confirmed'), ('done','Done'), ('cancel','Cancelled')], default='draft', tracking=True, string='status')
+     responsible_id = fields.Many2one('res.partner', string='Responsible') 
+     
+     def action_confirm(self):
+          self.state = 'confirm'
+          
+     def action_done(self):
+          self.state = 'done'
+          
+     def action_draft(self):
+          self.state = 'draft'
+          
+     def action_cancel(self):
+          self.state = 'cancel'
+          
+     @api.model 
+     def create(self, vals):
+          if not vals.get('note'):
+               vals['note'] = 'New Patient'
+          res = super(HospitalPatient, self).create(vals)
+          return res
